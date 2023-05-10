7066fda0384cfbb3205ddb854917f04afc4a2714
Arjun <arjunsethi33@gmail.com>
2023-05-10 14:52:10 +0530

    rotate

--- src/screens/Fish/Fish.js
+++ src/screens/Fish/Fish.js
@@ -8,23 +8,39 @@ const speed = 100;
 const fishHeight = 50;
 const fishWidth = 20;
 
+const newToValue = () => {
+  const minX = poundAreaWidth * 0.05;
+  const maxX = poundAreaWidth * 0.95;
+  const minY = poundAreaHeight * 0.05;
+  const maxY = poundAreaHeight * 0.9;
+
+  const x = Math.random() * (maxX - minX) + minX;
+  const y = Math.random() * (maxY - minY) + minY;
+
+  return {x, y};
+};
+
+const getNewAngle = (from, to) => {
+  const newAngle = `${
+    (Math.atan2(to.y - from.y, to.x - from.x) * 180) / Math.PI
+  }deg`;
+  return newAngle;
+};
+
 const FishModal = () => {
-  const newToValue = () => {
-    const minX = poundAreaWidth * 0.05;
-    const maxX = poundAreaWidth * 0.95;
-    const minY = poundAreaHeight * 0.05;
-    const maxY = poundAreaHeight * 0.9;
-
-    const x = Math.random() * (maxX - minX) + minX;
-    const y = Math.random() * (maxY - minY) + minY;
-
-    return {x, y};
-  };
-  const initialValue = newToValue();
-  const animation = useRef(
-    new Animated.ValueXY({x: initialValue.x, y: initialValue.y}),
+  const initialFromValue = newToValue();
+  const initialToValue = newToValue();
+  const initialAngle = getNewAngle(initialFromValue, initialToValue);
+  const translateAnimation = useRef(
+    new Animated.ValueXY({x: initialFromValue.x, y: initialFromValue.y}),
   ).current;
-
+  const rotationAnimation = useRef(new Animated.Value(0)).current;
+  const previousAngle = useRef(initialAngle);
+  let rotateY = rotationAnimation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [previousAngle, initialAngle],
+  });
+  console.log('rotateY', rotateY);
   const Animate = (from, to) => {
     const distance = Math.sqrt(
       Math.pow(to.x - from.x, 2) + Math.pow(to.y - from.y, 2),
@@ -32,33 +48,54 @@ const FishModal = () => {
     //constant relative duration for all distances
     const duration = (distance / speed) * 1000;
 
-    Animated.timing(animation, {
-      toValue: {x: to.x, y: to.y},
-      duration: duration,
-      easing: Easing.linear,
-      useNativeDriver: true,
-    }).start(({finished}) => {
+    const newAngle = getNewAngle(from, to);
+    rotateY = rotationAnimation.interpolate({
+      inputRange: [0, 1],
+      outputRange: [previousAngle, newAngle],
+    });
+    previousAngle.current = newAngle;
+    Animated.parallel([
+      Animated.timing(translateAnimation, {
+        toValue: {x: to.x, y: to.y},
+        duration: duration,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      }),
+      Animated.timing(rotationAnimation, {
+        toValue: 1,
+        duration: 300,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      }),
+    ]).start(({finished}) => {
       if (finished) {
         const newTo = newToValue();
+        rotationAnimation.setValue(0);
         Animate(to, newTo);
       }
     });
   };
 
   useEffect(() => {
-    Animate(initialValue, newToValue());
-  }, [animation]);
+    Animate(initialFromValue, initialToValue);
+  }, [translateAnimation]);
 
   return (
     <Animated.View
       style={{
-        backgroundColor: 'black',
         position: 'absolute',
         height: fishHeight,
         width: fishWidth,
-        transform: [{translateX: animation.x}, {translateY: animation.y}],
-      }}
-    />
+        backgroundColor: 'green',
+        transform: [
+          {translateX: translateAnimation.x},
+          {translateY: translateAnimation.y},
+          {rotateY: rotateY},
+        ],
+      }}>
+      {/* head */}
+      <View style={{height: '20%', backgroundColor: 'red'}} />
+    </Animated.View>
   );
 };
 
