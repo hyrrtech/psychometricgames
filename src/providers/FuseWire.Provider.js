e7d32ce3b2cde95e3e00e87bd2b9e90519057a8f
Arjun <arjunsethi33@gmail.com>
2023-05-28 21:18:19 +0530

    added functions package

--- src/providers/FuseWire.Provider.js
+++ src/providers/FuseWire.Provider.js
@@ -1,11 +1,12 @@
 import React, {createContext, useState, useEffect} from 'react';
 import {getHolderPositions, generateSequence} from '../utilities/FuseWire';
+import functions from '@react-native-firebase/functions';
 
 export const FuseWireContext = createContext();
 
 export const FuseWireProvider = ({children}) => {
   const {fuseHolderData, blankValues} = getHolderPositions(
-    generateSequence(2, 7),
+    generateSequence(1, 5),
   );
 
   const [fuseHolders, setFuseHolders] = useState(fuseHolderData);
