a4aee6a35c28f5cdac034e3205ccd6e5f94ab6f5
Arjun <arjunsethi33@gmail.com>
2023-08-04 09:12:23 +0530

    fixes

--- src/components/Masterpiece/Piece.js
+++ src/components/Masterpiece/Piece.js
@@ -5,7 +5,7 @@ import {useContext, useRef, useState, useEffect, useMemo} from 'react';
 import {MasterpieceContext} from '../../providers/Masterpiece.Provider';
 const {ratio} = constants;
 
-const Piece = ({pathD, viewBox, initialPosition, id}) => {
+const Piece = ({pathD, viewBox, initialPosition, id, pieceCorrectPositon}) => {
   const {
     positionsState,
     elementsData,
@@ -17,6 +17,7 @@ const Piece = ({pathD, viewBox, initialPosition, id}) => {
   const [strokeWidth, setStrokeWidth] = useState(pickedPieceId == id ? 3 : 2);
   const {x, y, width, height} = viewBox;
   const [pieceHeight, pieceWidth] = [height * ratio, width * ratio];
+  const [isAtCorrectPosition, setIsAtCorrectPosition] = useState(false);
   const calibratedInitialPosition = {
     x: initialPosition.x - pieceWidth / 2,
     y: initialPosition.y - pieceHeight / 2,
@@ -24,7 +25,6 @@ const Piece = ({pathD, viewBox, initialPosition, id}) => {
   const currentPositionId = useRef(null);
   const pan = useRef(new Animated.ValueXY(calibratedInitialPosition)).current;
   const colorAnimation = useRef(new Animated.Value(0)).current;
-
   const rotation = elementsData.find(
     element => element.id === id,
   ).pieceRotationAngle;
@@ -74,7 +74,19 @@ const Piece = ({pathD, viewBox, initialPosition, id}) => {
     onPanResponderRelease: (e, gesture) => {
       if (gesture.moveX === 0 && gesture.moveY === 0) return;
       pan.flattenOffset();
+
       const {exist, position} = isDropZone(gesture);
+
+      if (isAtCorrectPosition) {
+        Animated.spring(pan, {
+          toValue: {
+            x: pieceCorrectPositon.x - pieceWidth / 2,
+            y: pieceCorrectPositon.y - pieceHeight / 2,
+          },
+          useNativeDriver: false,
+        }).start();
+        return;
+      }
 
       if (exist) {
         //move to a valid position
@@ -101,6 +113,7 @@ const Piece = ({pathD, viewBox, initialPosition, id}) => {
       },
       useNativeDriver: false,
     }).start();
+
     if (currentPositionId.current !== null) {
       setPositionsState(prevState => {
         let newState = [...prevState];
@@ -131,6 +144,13 @@ const Piece = ({pathD, viewBox, initialPosition, id}) => {
         validPosition.isBlank
       ) {
         result = {exist: true, position: validPosition.position};
+
+        if (
+          pieceCorrectPositon.x === validPosition.position.x &&
+          pieceCorrectPositon.y === validPosition.position.y
+        ) {
+          setIsAtCorrectPosition(true);
+        }
 
         setPositionsState(prevState => {
           let newState = [...prevState];
